6ba96ee22c925e60fd7c82aaaac356a608f2ab77
neotekconsulting <jeffjohn@neotekconsulting.com>
2026-05-26 19:26:05 -0700

    Add base path for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://neotekconsulting.com',
+  base: '/neotekconsulting-site/',
   trailingSlash: 'always',
   build: {
     format: 'directory'
